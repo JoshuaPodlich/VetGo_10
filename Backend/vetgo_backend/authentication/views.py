003d2fba12512f0578bf3ca0bdffcc81580e66a0
Leo Stoyanov <leo.stoyanov24@gmail.com>
2024-02-06 23:41:41 -0600

    Setup and partial implementation of user registration.

--- Backend/vetgo_backend/authentication/views.py
+++ Backend/vetgo_backend/authentication/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import *
 
-# Create your views here.
+@ api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/auth/user-reg',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Initial creation of a VetGo user.' 
+        },
+    ]
+    return Response(routes)
+
+@api_view(['POST'])
+def createUser(request):
+    data = request.data
+    user = NotImplemented
+    if data['type_user'] == "vet_doc":
+        user = VeterinaryDoctor.objects.create(
+
+        )
+    else:
+        user = PetOwner.objects.create(
+            
+        )
+    # Create seralizer and file for it!
+    return Response()
