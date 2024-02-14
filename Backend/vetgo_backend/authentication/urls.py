a6ce2078f2907a0dc737b8c7597653fb71c55c64
Leo Stoyanov <leo.stoyanov24@gmail.com>
2024-02-13 19:35:20 -0600

    Deleted Python Django backend folder.

--- Backend/vetgo_backend/authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('routes', views.getRoutes),
-    path('users', views.getAllUsers),
-    path('create', views.createUser),
-    path('login', views.loginUser)
-]
