ba2746ea091d85a4baf786bd43d1d30713ec3f8c
Andy <ajbruder@iastate.edu>
2024-03-19 15:54:15 -0500

    re-done setup to fix build issues. beans taken from https://docs.spring.io/spring-authorization-server/reference/getting-started.html

--- AuthServer/src/test/java/auth/authserver/AuthServerApplicationTests.java
+++ AuthServer/src/test/java/auth/authserver/AuthServerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.auth;
+package auth.authserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuthApplicationTests {
+class AuthServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
